272911b793267b30f0359b37f2775251837561a9
MultiMouse <olmos.luisd@outlook.com>
2023-11-25 22:35:02 -0600

    added config and controllers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
 
 const SequelizedStore = require("connect-session-sequelize")(session.Store);
 
