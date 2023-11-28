d2f546886204aacaf8b999249eed3dcdaf188b12
MultiMouse <olmos.luisd@outlook.com>
2023-11-28 17:31:20 -0600

    added router connection to server.js, also seeds but idk

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 const sequelize = require("./config/connection");
+const routes = require("./controller");
 
 const SequelizedStore = require("connect-session-sequelize")(session.Store);
 
@@ -10,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "something idk...",
+  secret: "something idk.",
   cookie: {},
   resave: false,
   saveUnitialized: true,
@@ -21,9 +23,14 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
