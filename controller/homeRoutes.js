60acebcc76ccc18b3faf15b7e2f338b4b8f4234b
MultiMouse <olmos.luisd@outlook.com>
2023-11-28 17:36:04 -0600

    added api and user routes

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -4,4 +4,13 @@ router.get("/", async (req, res) => {
   res.render("homepage");
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
 module.exports = router;
