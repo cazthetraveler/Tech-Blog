fabe26fa45eb40adbdb7e625b490618e45452450
MultiMouse <olmos.luisd@outlook.com>
2023-11-29 19:56:28 -0600

    updated index in models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,18 @@
 const User = require("./User");
+const BlogPost = require("./BlogPost");
+const Comment = require("./Comment");
 
-module.exports = { User };
+BlogPost.belongsTo(User, {
+  foreignKey: "author_id",
+  as: "author_name",
+});
+
+BlogPost.hasMany(Comment, {
+  foreignKey: "blog_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, BlogPost, Comment };
