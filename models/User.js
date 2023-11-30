54786d8624550c30a5385cba9e508a09a98eaf9a
MultiMouse <olmos.luisd@outlook.com>
2023-11-29 19:54:38 -0600

    small change to the beforecreate function in user model

--- models/User.js
+++ models/User.js
@@ -38,7 +38,7 @@ User.init(
   },
   {
     hooks: {
-      beforeCreate: async (newUserData) => {
+      async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
